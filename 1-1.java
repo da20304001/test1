10a967905d3fe1b0bae63c20a0dc3d2dad3e895d
da20304001 <73629787+da20304001@users.noreply.github.com>
2020-10-29 14:41:20 +0900

    a
    b

---
+++ 1-1.java
@@ -0,0 +1,25 @@
+class Datebase{
+	String _sql;
+	Datebase(String s){
+		_sql=s;
+	}
+	void addSql(String s){
+		_sql=_sql.concat("\n"+s);
+	}
+	void display(){
+		System.out.println(_sql+";");
+	}
+	
+	public static void main(String args[]){
+		Datebase db=new Datebase("Select * from kokyaku");
+		
+		for(int i=0;i<args.length;i++){
+			db._sql=db._sql+args[i]+"\n";
+		}
+		
+		db.addSql("where kname '‹ã“VŽÐ'");
+		db.addSql("group by adress");
+		
+		db.display();
+	}
+}
